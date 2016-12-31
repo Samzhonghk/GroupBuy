7451918a0180ec05c05a43f8a380cbafb1fb483d
Samzhonghk <samzhongnz@gmail.com>
2016-12-31 19:41:54 +1300

    Initial Commit

---
+++ GroupBuy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GroupBuy
+//
+//  Created by ZhongMing on 31/12/16.
+//  Copyright © 2016 ZhongMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
